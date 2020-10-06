6149bf43b6eecb8fb60be3eeed5eb346e1b48256
mlq2 <70656673+mlq2@users.noreply.github.com>
2020-10-05 23:55:57 -0400

    Create errorHW.java

---
+++ classwork/src/errorHW.java
@@ -0,0 +1,6 @@
+I get the following error when trying to run my code 
+
+Error occurred during initialization of boot layer
+java.lang.LayerInstantiationException: Package jdk.internal.jimage.decompressor in both module java.base and module jrt.fs
+
+
